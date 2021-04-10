f824fd1698105e556d0ba2bf15b945a390b6cc7c
Jeff Hsieh <jeff1996176@gmail.com>
2021-04-10 12:06:40 +0800

    guess-num

--- 123.py
+++ 123.py
@@ -1,2 +1,11 @@
-name = input('please sign up your name')
-print('hi,'+ name)
+import random
+r=random.randint(1, 100)
+while True:
+	num = int(input('請猜數字: '))
+	if num > r:
+		print('比答案大')
+	elif num < r:
+		print('比答案小')
+	else:
+		print('終於猜對了!')
+		break
